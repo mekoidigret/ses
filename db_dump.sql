e0b7c392175c95b5c6bac41e8ea00a4449dacb2e
John Michael <manlupigjohnmichael@gmail.com>
2020-04-06 18:22:25 +0800

    Version 1.0.0

--- db_dump.sql
+++ db_dump.sql
@@ -13,10 +13,12 @@
 
 
 -- Dumping database structure for ses
+DROP DATABASE IF EXISTS `ses`;
 CREATE DATABASE IF NOT EXISTS `ses` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `ses`;
 
 -- Dumping structure for table ses.access_levels
+DROP TABLE IF EXISTS `access_levels`;
 CREATE TABLE IF NOT EXISTS `access_levels` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `type` varchar(50) NOT NULL,
@@ -31,6 +33,7 @@ INSERT INTO `access_levels` (`id`, `type`) VALUES
 /*!40000 ALTER TABLE `access_levels` ENABLE KEYS */;
 
 -- Dumping structure for table ses.addresses
+DROP TABLE IF EXISTS `addresses`;
 CREATE TABLE IF NOT EXISTS `addresses` (
   `user_id` int(10) unsigned NOT NULL,
   `street` varchar(150) NOT NULL,
@@ -44,10 +47,12 @@ CREATE TABLE IF NOT EXISTS `addresses` (
 -- Dumping data for table ses.addresses: ~0 rows (approximately)
 /*!40000 ALTER TABLE `addresses` DISABLE KEYS */;
 INSERT INTO `addresses` (`user_id`, `street`, `brgy`, `municipality`, `city`) VALUES
-	(2, 'Sta. Cruz', 'Lico-an', 'Barotac Nuevo', 'Iloilo');
+	(2, 'Sta. Cruz', 'Lico-an', 'Barotac Nuevo', 'Iloilo'),
+	(8, 'Sta. Cruz', 'Lico-an', 'Barotac Nuevo', 'Iloilo');
 /*!40000 ALTER TABLE `addresses` ENABLE KEYS */;
 
 -- Dumping structure for table ses.details
+DROP TABLE IF EXISTS `details`;
 CREATE TABLE IF NOT EXISTS `details` (
   `user_id` int(11) unsigned NOT NULL,
   `first_name` varchar(255) NOT NULL,
@@ -61,33 +66,56 @@ CREATE TABLE IF NOT EXISTS `details` (
   CONSTRAINT `FK_user_details_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table ses.details: ~0 rows (approximately)
+-- Dumping data for table ses.details: ~1 rows (approximately)
 /*!40000 ALTER TABLE `details` DISABLE KEYS */;
 INSERT INTO `details` (`user_id`, `first_name`, `last_name`, `gender`, `age`, `photo_id`) VALUES
-	(2, 'John Michael', 'Manlupig', 'Male', '20', 11);
+	(2, 'John Michael', 'Manlupig', 'Male', '21', 18),
+	(8, 'John Michael', 'Manlupig', 'Male', '21', 19);
 /*!40000 ALTER TABLE `details` ENABLE KEYS */;
 
 -- Dumping structure for table ses.files
+DROP TABLE IF EXISTS `files`;
 CREATE TABLE IF NOT EXISTS `files` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(11) unsigned NOT NULL,
   `name` varchar(255) NOT NULL,
   `size` int(11) NOT NULL,
   `type` varchar(50) NOT NULL,
+  `upload_dir` varchar(255) NOT NULL,
   `date` date NOT NULL,
   `time` time NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_files_users` (`user_id`),
   CONSTRAINT `FK_files_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 
--- Dumping data for table ses.files: ~0 rows (approximately)
+-- Dumping data for table ses.files: ~1 rows (approximately)
 /*!40000 ALTER TABLE `files` DISABLE KEYS */;
-INSERT INTO `files` (`id`, `user_id`, `name`, `size`, `type`, `date`, `time`) VALUES
-	(11, 2, 'Resume.jpg', 9148928, 'image/jpeg', '2020-03-16', '18:18:47');
+INSERT INTO `files` (`id`, `user_id`, `name`, `size`, `type`, `upload_dir`, `date`, `time`) VALUES
+	(18, 2, '36276951_1912975805392180_2582493943081992192_n.jpg', 8678, 'image/jpeg', '..\\assets\\files\\images\\pps', '2020-03-21', '14:10:21'),
+	(19, 8, 'Resume.jpg', 9148928, 'image/jpeg', '..\\assets\\files\\images\\pps', '2020-03-25', '22:28:16');
 /*!40000 ALTER TABLE `files` ENABLE KEYS */;
 
+-- Dumping structure for table ses.requests
+DROP TABLE IF EXISTS `requests`;
+CREATE TABLE IF NOT EXISTS `requests` (
+  `id` varchar(19) NOT NULL,
+  `date` date NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `passed` tinyint(1) unsigned NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `FK_requests_users` (`user_id`),
+  CONSTRAINT `FK_requests_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table ses.requests: ~1 rows (approximately)
+/*!40000 ALTER TABLE `requests` DISABLE KEYS */;
+INSERT INTO `requests` (`id`, `date`, `user_id`, `passed`) VALUES
+	('2020-98bf-6a6e-68df', '2020-03-29', 8, 1);
+/*!40000 ALTER TABLE `requests` ENABLE KEYS */;
+
 -- Dumping structure for table ses.users
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `access_level_id` int(10) unsigned NOT NULL,
@@ -97,13 +125,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `username` (`username`),
   KEY `FK_users_access_levels` (`access_level_id`),
   CONSTRAINT `FK_users_access_levels` FOREIGN KEY (`access_level_id`) REFERENCES `access_levels` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 -- Dumping data for table ses.users: ~3 rows (approximately)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`id`, `access_level_id`, `username`, `password`) VALUES
 	(1, 2, 'mekkyinblack', '$2y$10$25sdXcIs94Nah/n/YRlY0.Diij5A.8ptuNrx2yTA84lcUUjEFPJHW'),
-	(2, 2, 'admin', '$2y$10$j1Mjg8aeQNfoXkTHMC07IOiLTLf6BGDQBv.6ZCYM2PFJuC//kEHZa');
+	(2, 2, 'admin', '$2y$10$j1Mjg8aeQNfoXkTHMC07IOiLTLf6BGDQBv.6ZCYM2PFJuC//kEHZa'),
+	(8, 1, 'student', '$2y$10$pf7Nc3Tf7fDNT11J/gbi0.6nxfLGbynImWoLHfGK7zopGT1YSQzmO');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
